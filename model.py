93fb674318641d79af6927872e5f3a3631daeaf4
Fernandovilelab <fernando.vilelab@gmail.com>
2025-04-28 18:06:37 -0300

    Commit 9: Replacing conditional with polymorphism - part 2.

--- model.py
+++ model.py
@@ -3,10 +3,9 @@ class Price:
         pass
 
     def get_frequent_renter_points(self, days_rented: int) -> int:
-        return 1  # regra padrão: sempre 1 ponto
+        return 1  
 
-
-class RegularPrice(Price):
+class RegulaPrice(Price):
     def get_charge(self, days_rented: int) -> float:
         amount = 2
         if days_rented > 2:
@@ -40,22 +39,19 @@ class Book:
 
     def __init__(self, title: str, price_code: int):
         self.title = title
-        self.set_price_code(price_code)
-
-    def set_price_code(self, price_code: int):
-        if price_code == Book.REGULAR:
-            self.price = RegularPrice()
-        elif price_code == Book.NEW_RELEASE:
-            self.price = NewReleasePrice()
+        self.price = self.create_price(price_code)
+    
+    def create_price(self, price_code: int):
+        if price_code == Book.NEW_RELEASE:
+            return NewReleasePrice()
         elif price_code == Book.CHILDREN:
-            self.price = ChildrenPrice()
-        else:
-            raise ValueError("Invalid price code")
-
-    def get_charge(self, days_rented: int) -> float:
+            return ChildrenPrice()
+        return RegulaPrice()  
+    
+    def get_charge(self, days_rented: int):
         return self.price.get_charge(days_rented)
 
-    def get_frequent_renter_points(self, days_rented: int) -> int:
+    def get_frequent_renter_points(self, days_rented: int):
         return self.price.get_frequent_renter_points(days_rented)
 
 
